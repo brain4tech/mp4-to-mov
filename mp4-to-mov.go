d0a1dc1d5ad7b2502cf97aa96ef981987c1e4f67
Brain4Tech <brain4techyt@gmail.com>
2022-02-23 15:58:32 +0100

    updated folder structure

--- mp4-to-mov.go
+++ mp4-to-mov.go
@@ -5,7 +5,7 @@ Copyright © 2022 Brain4Tech <brain4techyt@gmail.com>
 
 package main
 
-import "github.com/Brain4Tech/mp4-to-mov/cmd"
+import "github.com/Brain4Tech/mp4-to-mov/src/cmd"
 
 func main() {
 	cmd.Execute()
